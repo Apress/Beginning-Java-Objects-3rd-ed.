832b6737239fcc50bc5bd2b0436022a5b385d145
ApressGryffinWinkler <104927373+ApressGryffinWinkler@users.noreply.github.com>
2023-03-07 09:17:58 -0500

    Add files via upload

---
+++ EnrollmentStatus.java
@@ -0,0 +1,23 @@
+// Used by the Section class to represent various possible outcomes of
+// an attempt to enroll by a Student.
+
+public enum EnrollmentStatus {
+	// Enumerate the values that the enum can assume.
+	success("Enrollment successful!  :o)"), 
+	secFull("Enrollment failed;  section was full.  :op"), 
+	prereq("Enrollment failed; prerequisites not satisfied.  :op"), 
+	prevEnroll("Enrollment failed; previously enrolled.  :op");
+
+	// This represents the value of an enum instance.
+	private final String value;
+
+	// A "constructor" of sorts (used above).
+	EnrollmentStatus(String value) {
+		this.value = value;
+	}
+
+	// Accessor for the value of an enum instance.
+	public String value() {
+		return value;
+	}
+}
---
+++ READ_ME.txt
@@ -0,0 +1,1 @@
+This is the source code that accompanies Chapter 14.
